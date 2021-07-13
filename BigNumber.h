decc7b16fa95504e179c4de50600c9c293f737f9
yuofnc <34058163+yuofnc@users.noreply.github.com>
2021-07-13 22:22:47 +0800

    Update BigNumber.h

--- BigNumber.h
+++ BigNumber.h
@@ -10,25 +10,30 @@ class CBigNumber
 public:
 	CBigNumber();
 	CBigNumber(long lSize);
+	CBigNumber(CBigNumber &cNumSource);
 	virtual ~CBigNumber();
 	void setNumber(unsigned long lset);
-
+	CBigNumber copyNumber(CBigNumber &cNumSource);
 	void  Add(unsigned long lAdd);
+	void  Add(CBigNumber &cNumSource);
 	void  Mul(unsigned long lMul);
+	void  Mul(CBigNumber &cNumSource);
 
 	void MulMAXITEM(unsigned int iTimes);
 	//void  Mul(CBigNumber ba, unsigned long lMul);
 	long getValidCount();
+	long getUsedCount();
+	void SetValidCount(unsigned long uSet);
 	void printResu();
 public:
 	unsigned long * pUsed;
 protected:
 	void reNew(long lSize);
 	void reNewAndSet(CBigNumber nPrev);
+	void reNewAndSet(long lSize);
 	void reNewAndSet();
 private:
 	
 	unsigned long pUsedCount;  //占用位
 	unsigned long pValidCount; //已用位
 };
-
